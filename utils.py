bfab03ecdb6b8578505f474220761c33a826ea8c
Anoop Kunchukuttan <ankunchu@microsoft.com>
2019-10-15 04:35:13 -0700

    changes to translate, added evaluation

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import numpy as np
-import cupy as cp
+#import cupy as cp
 import embeddings
 import scipy.linalg
 import collections 
@@ -9,6 +9,7 @@ import os
 from subprocess import Popen, PIPE, STDOUT
 import shlex
 import numpy as np
+import itertools as it
 
 
 def read_model(mapping_model_dir):
@@ -73,7 +74,7 @@ def map_embedding_db(in_emb_fname, out_emb_fname, vocab_type, mapping_model_dir,
     with open(out_emb_fname,'w',encoding='utf-8') as outfile:
         embeddings.write(src_words,xw,outfile)
 
-def translate(src_emb_fname, tgt_emb_fname, trans_tgt_fname, trans_src_fname=None, retrieval_method="csls", csls_k=10, batch_size=2500):
+def translate(src_emb_fname, tgt_emb_fname, trans_tgt_fname=None, trans_src_fname=None, retrieval_method="csls", csls_k=10, batch_size=2500):
 
     print('Loading train data...')
 
@@ -98,7 +99,8 @@ def translate(src_emb_fname, tgt_emb_fname, trans_tgt_fname, trans_src_fname=Non
     trans_words=[]
     trans_idx=[]
     oov=set()
-    if trans_src_fname is not None:
+    #if trans_src_fname is not None:
+    if isinstance(trans_src_fname, str):
         with open(trans_src_fname, 'r', encoding='utf-8', errors='surrogateescape') as trans_src_file:
             for line in trans_src_file:
                 try:
@@ -110,6 +112,16 @@ def translate(src_emb_fname, tgt_emb_fname, trans_tgt_fname, trans_src_fname=Non
                     trans_idx.append(w_ind)
                 except KeyError:
                     oov.add(w)
+    elif isinstance(trans_src_fname, list):
+        for w in trans_src_fname:
+            try:
+                all_words.append(w)
+                w_ind = src_word2ind[w]
+                trans_words.append(w)
+                trans_idx.append(w_ind)
+            except KeyError:
+                oov.add(w)
+
     else:                            
         all_words=src_words
         trans_words=src_words
@@ -187,13 +199,23 @@ def translate(src_emb_fname, tgt_emb_fname, trans_tgt_fname, trans_src_fname=Non
         print('Completed in {0} seconds'.format(time.time()-t))
 
    
+    # get translations
+    trans_pairs=[]
+    for w in trans_words: 
+        trans=''
+        if w in src_word2ind:
+            trans=tgt_words[translation[src_word2ind[w]]]
+        if len(trans)>0 or trans_tgt_fname is not None:
+            ### include blank lines only in the case of writing output to file 
+            trans_pairs.append((w,trans))
+
     ### write the translations (1 pair per line format)
-    with open(trans_tgt_fname, 'w', encoding='utf-8', errors='surrogateescape') as trans_tgt_file:
-        for w in trans_words: 
-            trans=''
-            if w in src_word2ind:
-                trans=tgt_words[translation[src_word2ind[w]]]
-            trans_tgt_file.write('{}\t{}\n'.format(w,trans))
+    if trans_tgt_fname is not None:
+        with open(trans_tgt_fname, 'w', encoding='utf-8', errors='surrogateescape') as trans_tgt_file:
+            for w, trans in trans_pairs:
+                trans_tgt_file.write('{}\t{}\n'.format(w,trans))
+    else:
+        return dict(trans_pairs)
    
     ### output in moses format 
     #with open(trans_tgt_fname, 'w', encoding='utf-8') as trans_tgt_file:
@@ -206,8 +228,153 @@ def translate(src_emb_fname, tgt_emb_fname, trans_tgt_fname, trans_src_fname=Non
     #            trans_tgt_file.write( u'{} ||| {} ||| {} ||| {}\n'.format( 
     #                            wno, '@@OOV@@', '0.0 0.0 0.0 0.0', 0.0) )
 
-def read_dict(fname,delim=' '):
-    trans_dict={}
+def evaluate(src_emb_fname, tgt_emb_fname, dict_fname, max_voc=0, retrieval_method="csls", csls_k=10, batch_size=2500):
+
+    print('Loading train data...')
+
+    srcfile=open(src_emb_fname, 'r', encoding='utf-8', errors='surrogateescape')
+    tgtfile=open(tgt_emb_fname, 'r', encoding='utf-8', errors='surrogateescape')
+
+    # Read source embeddings
+    src_words, x = embeddings.read(srcfile,max_voc=max_voc, dtype='float32')
+    src_word2ind = {word: i for i, word in enumerate(src_words)}
+    
+    # Read target embeddings
+    tgt_words, z = embeddings.read(tgtfile,max_voc=max_voc, dtype='float32')
+    tgt_word2ind = {word: i for i, word in enumerate(tgt_words)}
+   
+    srcfile.close()
+    tgtfile.close()
+
+    xw = embeddings.length_normalize(x)
+    zw = embeddings.length_normalize(z)
+
+
+    # Loading test dictionary
+    f = open(dict_fname, encoding='utf-8', errors='surrogateescape')
+    src2trg = collections.defaultdict(set)
+    trg2src = collections.defaultdict(set)
+    oov = set()
+    vocab = set()
+    for line in f:
+        src, trg = line.split()
+        if args.max_vocab:
+            src=src.lower()
+            trg=trg.lower()
+        try:
+            src_ind = src_word2ind[src]
+            trg_ind = trg_word2ind[trg]
+            src2trg[src_ind].add(trg_ind)
+            trg2src[trg_ind].add(src_ind)
+            vocab.add(src)
+        except KeyError:
+            oov.add(src)
+    src = list(src2trg.keys())
+    trgt = list(trg2src.keys())
+
+    oov -= vocab  # If one of the translation options is in the vocabulary, then the entry is not an oov
+    coverage = len(src2trg) / (len(src2trg) + len(oov))
+    f.close()
+
+    ### get translations 
+    translation = collections.defaultdict(int)
+    translation5 = collections.defaultdict(list)
+    translation10 = collections.defaultdict(list)
+
+    if retrieval_method == 'nn':  # Standard nearest neighbor
+        for i in range(0, len(src), batch_size):
+            j = min(i + batch_size, len(src))
+            similarities = xw[src[i:j]].dot(zw.T)
+            nn = similarities.argmax(axis=1).tolist()
+            similarities_idx = similarities.argsort(axis=1)
+            nn5 = similarities_idx[:,-5:]
+            nn10 = similarities_idx[:,-10:]
+
+            for k in range(j-i):
+                translation[src[i+k]] = nn[k]
+                translation5[src[i+k]] = nn5[k]
+                translation10[src[i+k]] = nn10[k]
+                
+    elif retrieval_method == 'csls':
+        t=time.time()
+        nbrhood_x=np.zeros(xw.shape[0])
+        nbrhood_z=np.zeros(zw.shape[0])
+        nbrhood_z2=cp.zeros(zw.shape[0])
+        print('Computing X Neighbourhood')
+        # batch_size=1000
+        for i in range(0, len(src), batch_size):
+            j = min(i + batch_size, len(src))
+            similarities = xw[src[i:j]].dot(zw.T)
+            # similarities_x = np.sort(similarities, axis=1)
+            similarities_x = -1*np.partition(-1*similarities,csls_k-1 ,axis=1)
+            #similarities_x = -1*cp.partition(-1*cp.dot(cp.asarray(xw[src[i:j]]),cp.transpose(cp.asarray(zw))),csls_k-1 ,axis=1)[:,:csls_k]
+            nbrhood_x[src[i:j]]=np.mean(similarities_x[:,:csls_k],axis=1)
+        print('Completed in {0} seconds'.format(time.time()-t))
+        print('Computing Z Neighbourhood')
+
+        batch_num=1
+        for i in range(0, zw.shape[0], batch_size):
+            j = min(i + batch_size, zw.shape[0])
+            similarities = -1*cp.partition(-1*cp.dot(cp.asarray(zw[i:j]),cp.transpose(cp.asarray(xw))),csls_k-1 ,axis=1)[:,:csls_k]
+            nbrhood_z2[i:j]=(cp.mean(similarities[:,:csls_k],axis=1))
+            print('Completed batch {0} in {1}'.format(batch_num, time.time()-t))
+            batch_num+=1
+        # gc.collect()
+        # t=time.time()
+        nbrhood_z=cp.asnumpy(nbrhood_z2)
+        # ipdb.set_trace()
+        print(time.time()-t)
+        csls_alpha=1
+        for i in range(0, len(src), batch_size):
+            j = min(i + batch_size, len(src))
+            similarities = xw[src[i:j]].dot(zw.T)
+            similarities = np.transpose(np.transpose(2*similarities) - csls_alpha*nbrhood_x[src[i:j]])- csls_alpha*nbrhood_z
+            nn = similarities.argmax(axis=1).tolist()
+            print(time.time()-t)
+            similarities = np.argsort((similarities),axis=1)
+
+            nn5 = (similarities[:,-5:])
+            nn10 = (similarities[:,-10:])
+            for k in range(j-i):
+                translation[src[i+k]] = nn[k]
+                translation5[src[i+k]] = nn5[k]
+                translation10[src[i+k]] = nn10[k]
+        print('Completed in {0} seconds'.format(time.time()-t))
+
+   
+    #### write the translations (1 pair per line format)
+    #with open(trans_tgt_fname, 'w', encoding='utf-8', errors='surrogateescape') as trans_tgt_file:
+    #    for w in trans_words: 
+    #        trans=''
+    #        if w in src_word2ind:
+    #            trans=tgt_words[translation[src_word2ind[w]]]
+    #        trans_tgt_file.write('{}\t{}\n'.format(w,trans))
+
+    # evaluation metrics 
+    accuracy = np.mean([1 if translation[i] in src2trg[i] else 0 for i in src])
+    mean=0
+    for i in src:
+        for k in translation5[i]:
+            if k in src2trg[i]:
+                mean+=1
+                break
+
+    mean/=len(src)
+    accuracy5 = mean
+
+    mean=0
+    for i in src:
+        for k in translation10[i]:
+            if k in src2trg[i]:
+                mean+=1
+                break
+
+    mean/=len(src)
+    accuracy10 = mean
+    print('Coverage:{0:7.2%}  Accuracy:{1:7.2%}  Accuracy(Top 5):{2:7.2%}  Accuracy(Top 10):{3:7.2%}'.format(coverage, accuracy, accuracy5, accuracy10))
+
+def read_bilingual_dict(fname,delim=' '):
+    trans_dict=collections.defaultdict(set)
     with open(fname,'r',encoding='utf-8') as infile:
         for i,line in enumerate(infile,1):
             r=line.strip().split(delim)
@@ -215,7 +382,7 @@ def read_dict(fname,delim=' '):
                 print('Ignore entry on line {}: does not have 2 fields'.format(i))
                 continue
             k,v=r
-            trans_dict[k]=v
+            trans_dict[k].add(v)
 
     return trans_dict
 
@@ -274,8 +441,8 @@ def add_oov_embeddings(train_dict_fname, test_dict_fname,
     """
     
     ## read dictionaries
-    train_dict = read_dict(train_dict_fname)    
-    test_dict  = read_dict(test_dict_fname)
+    train_dict = read_bilingual_dict(train_dict_fname)    
+    test_dict  = read_bilingual_dict(test_dict_fname)
     
     # read embeddings 
     src_vcb_words=None
@@ -296,8 +463,9 @@ def add_oov_embeddings(train_dict_fname, test_dict_fname,
     print('Number of src OOV words: {}'.format(len(src_oov_words)))
     
     tgt_oov_words=set()
-    tgt_oov_words.update(train_dict.values())
-    tgt_oov_words.update(test_dict.values())   
+    
+    tgt_oov_words.update(it.chain(train_dict.values()))
+    tgt_oov_words.update(it.chain(test_dict.values()))
     tgt_oov_words.difference_update(tgt_vcb_words) 
     print('Number of tgt OOV words: {}'.format(len(tgt_oov_words)))
     
